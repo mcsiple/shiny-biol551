cd5eac1f86eb6369a909711dc9d805b56f6baa5c
mcsiple <mcsiple@gmail.com>
2021-04-05 22:59:56 -0700

    add dashboard example

---
+++ dash/app.R
@@ -0,0 +1,65 @@
+# Example 03: Shiny dashboards
+
+library(tidyverse)
+library(shiny)
+library(shinydashboard)
+library(palmerpenguins)
+
+# Define UI for application that draws a histogram
+ui <- dashboardPage(
+  dashboardHeader(title = "We love penguins!"),
+  dashboardSidebar(
+    menuItem("Bill length & depth", tabName = "lendep", icon = icon("drafting-compass")),
+    menuItem("Body mass ", tabName = "bodymass", icon = icon("crow"))
+  ),
+  dashboardBody(
+      tabItems(
+    tabItem(
+      "lendep", 
+      fluidPage(
+          h1("Bil length and depth"),
+      box(selectInput("species", "Species:", unique(penguins$species)), width = 8),
+      box(plotOutput("correlation_plot"), width = 8)
+    )
+    ),
+    tabItem(
+      "bodymass", 
+      fluidPage(h1("Body mass"),
+      box(selectInput("sex", "Sex:", c("female", "male")), width = 8),
+      box(plotOutput("bodymass_plot"), width = 8)
+    ) # /fluidPage
+    )# /tabItem
+  ) # /tabItems
+  )
+)
+
+# Define server logic required to draw a histogram
+server <- function(input, output) {
+  output$correlation_plot <- renderPlot({
+    penguin_species <- filter(
+      penguins,
+      species == input$species & !is.na(sex)
+    )
+
+    ggplot(penguin_species, aes(x = bill_length_mm, y = bill_depth_mm, colour = sex)) +
+      geom_point(size = 3) +
+      scale_color_brewer() +
+      theme_classic(base_size = 14)
+  })
+
+  output$bodymass_plot <- renderPlot({
+    penguin_sex <- filter(
+      penguins,
+      sex == input$sex & !is.na(species)
+    )
+
+    ggplot(penguin_sex, aes(x = body_mass_g, colour = species, fill = species)) +
+      geom_density(size = 3) +
+      scale_color_brewer(palette = 2) +
+      scale_fill_brewer(palette = 2) +
+      theme_classic(base_size = 14)
+  })
+}
+
+# Run the application
+shinyApp(ui = ui, server = server)
